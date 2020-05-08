a56231ad63f6cfe28114a6046fedcbc190a4c497
Iotwo <integrationoftwo@gmail.com>
2020-05-08 11:58:55 +0700

    Added Process BMP Info function

--- imgexctractor.c
+++ imgexctractor.c
@@ -12,19 +12,22 @@ typedef enum{
 } eImageTypes_t;
 struct RawImage{
 	eImageTypes_t imgType;
-	unsigned int Xsize;
-	unsigned int Ysize;
-	unsigned short int bytesPerPixel;
-	char* imgBitmapVector;
+	unsigned long int Xsize;
+	unsigned long int Ysize;
+	unsigned char bytesPerPixel;
+	unsigned char* imgBitmapVector;
 } extractedPic;
 
 eImageTypes_t CheckFileType(FILE* const openedFile);
 unsigned long long int CheckBMPSizeAndConsist(FILE* const openedFile);
-unsigned int InverseByteOrder(unsigned int length, unsigned char* vector);
+void GetBMPFileInfo(FILE* const openedFile, struct RawImage imgDataKeeper);
+unsigned char ProcessBMPInfo(FILE* const openedFile, struct RawImage infoStructure); //not ready
+//unsigned char* ExtractBitmap(FILE* const openedFile); //not ready
+unsigned int InverseByteOrder(unsigned char* vector, unsigned int vectorLength);
 unsigned long int ConvertHexToULI(unsigned char* inputHexString, unsigned char strLength);
 
 int main(int argc, char** argv){
-	FILE* inputFile;
+	FILE* restrict inputFile;
 	if(argc<=1) printf("No arguments supplied. Exiting."); // do nothing
 	if(argc>1){
 		printf("Output path not mentioned. Output will be stored in the same directory named: out+%s.\n",argv[1]);
@@ -34,14 +37,14 @@ int main(int argc, char** argv){
       printf("Extracted type:%d\n",extractedPic.imgType);
       if(extractedPic.imgType==BMP){ // maybe case?
         if(CheckBMPSizeAndConsist(inputFile)!=0){
-
+          ProcessBMPInfo(inputFile,extractedPic);
+          //redraw 
         }
         else{
           printf("Error in file consistency.\n");
           exit(EXIT_FAILURE);
         } 
-        // extract the header to read the file
-        // extract the bitmap  
+        
       }
       //do the same for other formats later
 		}
@@ -49,7 +52,6 @@ int main(int argc, char** argv){
       perror("Error occured while opening file: ");
       exit(ferror(inputFile));
     }
-    
 	}
   exit(EXIT_SUCCESS);
 }
@@ -109,54 +111,96 @@ eImageTypes_t CheckFileType(FILE* const openedFile){
 
   return imgType;
 }
+
 unsigned long long int CheckBMPSizeAndConsist(FILE* const openedFile){
   //checks size of BMP-file reading it byte by byte
   //0 - something went wrong.
-
   unsigned long long int fileBytesCount=0; //how large the file is
   unsigned long long int i=0;
+  int c='\0';
   unsigned char * restrict bmFileSizeField; //readed data
   bmFileSizeField=(unsigned char*)calloc(5,sizeof(unsigned char));
   
   (void)fseek(openedFile,2,SEEK_SET);
-  printf("filling array\n");
   while(i<4){
     *(bmFileSizeField+i)=(unsigned char)fgetc(openedFile);
     i++;
   }
   *(bmFileSizeField+i)='\0';
-  //InverseByteOrder(i,bmFileSizeField); // 4 is to avoid EOL-marker switching
   fileBytesCount=ConvertHexToULI(bmFileSizeField, 4);
+  
   (void)rewind(openedFile);
   i=0;
-  printf("check sizes; EOF is %d\n",EOF);
-  while(fgetc(openedFile)!=EOF) printf("%d;",i++);
-  printf("checked=%d;\n",i);
-  printf("comparing...\n");
+  printf("EOF=%d\n",EOF);
+  //while(fgetc(openedFile)!=EOF) i++;
+  while((c=fgetc(openedFile))!=EOF && feof(openedFile)==0 && ferror(openedFile)==0) i++;
+  printf("feof=%x, ferror=%x, error=%x symbol=%d\n", feof(openedFile),ferror(openedFile),errno, c);
+  if(c==EOF) printf("Char = EOF!\n");
+  //printf("err=%d\n",feof(openedFile));
+  printf("Count: %lld, field: %lld;\n",i,fileBytesCount);
   if(i==fileBytesCount) return fileBytesCount;
   else return 0;
 }
 
+unsigned char ProcessBMPInfo(FILE* const openedFile, struct RawImage infoStructure){
+  //find and read the file's header
+  // -copy datavector
+  const unsigned char dataFieldLen=4;
+  unsigned char* restrict picData;
+  picData = (unsigned char*)calloc(4,sizeof(unsigned char));
+  unsigned long int i=0, j=0, dataBytesCnt=0;
+  (void)rewind(openedFile);
+  //get width, heigth, color depth
+  (void)fseek(openedFile,0x12,SEEK_SET);
+  for(i=0;i<dataFieldLen;i++) *(picData+i)=fgetc(openedFile);
+  infoStructure.Xsize=ConvertHexToULI(picData,dataFieldLen);
+  for(i=0;i<dataFieldLen;i++) *(picData+i)=fgetc(openedFile);
+  infoStructure.Ysize=ConvertHexToULI(picData,dataFieldLen);
+  (void)fseek(openedFile,0x1C,SEEK_SET);// skip biPlanes field
+  for(i=0;i<dataFieldLen-2;i++) *(picData+i)=fgetc(openedFile);
+  infoStructure.bytesPerPixel=ConvertHexToULI(picData,dataFieldLen-2)/8;
+  
+  //copy color vector
+  (void)fseek(openedFile,0xA,SEEK_SET);
+  for(i=0;i<dataFieldLen;i++) *(picData+i)=fgetc(openedFile);
+  i=ConvertHexToULI(picData,dataFieldLen);
+  dataBytesCnt=infoStructure.Xsize*infoStructure.Ysize*infoStructure.bytesPerPixel;
+  infoStructure.imgBitmapVector=(unsigned char*)calloc(dataBytesCnt, sizeof(unsigned char));
+  (void)fseek(openedFile,i,SEEK_SET);
+  for(j=0; feof(openedFile)==0; j++) *(infoStructure.imgBitmapVector+j)=(unsigned char)fgetc(openedFile);
+    //debug info
+  printf("X: %d\nY: %d\nD: %d\nA: %x\n", infoStructure.Xsize, infoStructure.Ysize, infoStructure.bytesPerPixel,i);
+
+  return 0;
+}
+
+//=======================================================================
+unsigned char* ExtractBitmap(FILE* const openedFile, unsigned int imageVectorSize){
+  unsigned char* restrict bitmapVector;
+  bitmapVector=(unsigned char*)calloc(imageVectorSize, sizeof(unsigned char));
+
+  return bitmapVector;
+}
+
 unsigned long int ConvertHexToULI(unsigned char* inputHexString, unsigned char strLength){
-  const unsigned char base=0xf;
+  const unsigned char base=0xff;
   unsigned char i=0;
   unsigned long int result=0;
 
   while(i<strLength){
-    result+=*(inputHexString+i)*pow(base,i);
+    result+=*(inputHexString+i)*pow(base+1,i);
     i++;
   }
 
   return result;
 }
 
-unsigned int InverseByteOrder(unsigned int length, unsigned char* vector){
+unsigned int InverseByteOrder(unsigned char* vector, unsigned int vectorLength){
   unsigned int byteCnt=0;
-  for(;byteCnt<length/2;byteCnt++){
-    //printf("START: BMPfile-%d; pair-%d\n",*(vector+i),*(vector+length-1-i));
-    vector[byteCnt]=*(vector+byteCnt)+*(vector+length-1-byteCnt); //printf("ACT1: BMPfile-%d; pair-%d\n",*(vector+i),*(vector+length-1-i));
-    *(vector+length-1-byteCnt)=*(vector+byteCnt)-*(vector+length-1-byteCnt); //printf("ACT2: BMPfile-%d; pair-%d\n",*(vector+i),*(vector+length-1-i));
-    *(vector+byteCnt)=*(vector+byteCnt)-*(vector+length-1-byteCnt); //printf("ACT3-END: BMPfile-%d; pair-%d\n",*(vector+i),*(vector+length-1-i));
+  for(;byteCnt<vectorLength/2;byteCnt++){
+    vector[byteCnt]=*(vector+byteCnt)+*(vector+vectorLength-1-byteCnt);
+    *(vector+vectorLength-1-byteCnt)=*(vector+byteCnt)-*(vector+vectorLength-1-byteCnt);
+    *(vector+byteCnt)=*(vector+byteCnt)-*(vector+vectorLength-1-byteCnt);
   }
   return byteCnt;
 }
